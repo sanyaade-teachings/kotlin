cc23d7771809331cc341c7c64139e6f06d4b7e24
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2025-03-04 22:01:21 +0100

    fixup! Fir: Support @JvmExposeBoxed annotation

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/PhasedJvmDiagnosticPsiTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/PhasedJvmDiagnosticPsiTestGenerated.java
@@ -43940,23 +43940,6 @@ public class PhasedJvmDiagnosticPsiTestGenerated extends AbstractPhasedJvmDiagno
       public void testVarargsOnParametersOfValueClassType() {
         runTest("compiler/testData/diagnostics/tests/valueClasses/varargsOnParametersOfValueClassType.kt");
       }
-
-      @Nested
-      @TestMetadata("compiler/testData/diagnostics/tests/valueClasses/jvmExposeBoxed")
-      @TestDataPath("$PROJECT_ROOT")
-      public class JvmExposeBoxed {
-        @Test
-        @TestMetadata("jvmName.kt")
-        public void testJvmName() {
-          runTest("compiler/testData/diagnostics/tests/valueClasses/jvmExposeBoxed/jvmName.kt");
-        }
-
-        @Test
-        @TestMetadata("simple.kt")
-        public void testSimple() {
-          runTest("compiler/testData/diagnostics/tests/valueClasses/jvmExposeBoxed/simple.kt");
-        }
-      }
     }
 
     @Nested
@@ -55063,6 +55046,23 @@ public class PhasedJvmDiagnosticPsiTestGenerated extends AbstractPhasedJvmDiagno
       @TestMetadata("reservedMethodsInInterfaces.kt")
       public void testReservedMethodsInInterfaces() {
         runTest("compiler/fir/analysis-tests/testData/resolve/inlineClasses/reservedMethodsInInterfaces.kt");
+      }
+
+      @Nested
+      @TestMetadata("compiler/fir/analysis-tests/testData/resolve/inlineClasses/jvmExposeBoxed")
+      @TestDataPath("$PROJECT_ROOT")
+      public class JvmExposeBoxed {
+        @Test
+        @TestMetadata("jvmName.kt")
+        public void testJvmName() {
+          runTest("compiler/fir/analysis-tests/testData/resolve/inlineClasses/jvmExposeBoxed/jvmName.kt");
+        }
+
+        @Test
+        @TestMetadata("simple.kt")
+        public void testSimple() {
+          runTest("compiler/fir/analysis-tests/testData/resolve/inlineClasses/jvmExposeBoxed/simple.kt");
+        }
       }
     }
 
